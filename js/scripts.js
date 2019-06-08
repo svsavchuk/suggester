ed10a1f030c8d7da1abbcb8c4ca4a6d27ccaf8aa
Serhiy Savchuk <svsavchuk@hotmail.com>
2019-06-07 19:53:24 -0700

    Add new paragraph to index.html and IF statements to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,27 @@
 $(document).ready(function(){
   $("#form-one").submit(function(){
 
-      var waffles =
-      $("#waffles").val();
+    var waffles =
+    $("#waffles").val();
 
-      var experience =
-      $("#experience").val();
+    var experience =
+    $("#experience").val();
 
-      var platform =
-      $("#platform").val();
+    var platform =
+    $("#platform").val();
 
-      var os =
-      $("#os").val();
+    var os =
+    $("#os").val();
 
-      var frontbackend =
-      $("#frontbackend").val();
+    var frontbackend =
+    $("#frontbackend").val();
 
-      
-
-      alert("Answer is "+frontbackend);
-
+    if (waffles === "yes") {
+      alert("Waffles is yes");
+    } else {
+      alert("Waffles is no");
+    }
+    // alert("Answer is: "+waffles+" "+platform+" "+os+" "+frontbackend+".");
 
     event.preventDefault();
   });
